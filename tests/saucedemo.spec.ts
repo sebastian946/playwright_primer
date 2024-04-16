6491b7fd009b8fb751d221dd95dcf81c9c6900ad
sebastian946 <jhoan1113@hotmail.com>
2024-04-16 06:23:01 -0500

    Main code playwright

---
+++ tests/saucedemo.spec.ts
@@ -0,0 +1,55 @@
+import { test, expect } from "@playwright/test";
+
+test("purchase an item", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/v1/");
+
+  await page.getByRole("textbox", { name: "Username" }).fill("standard_user");
+
+  await page.getByRole("textbox", { name: "Password" }).fill("secret_sauce");
+  await page.getByRole("button", { name: "LOGIN" }).click();
+
+  const items = await page
+    .locator("#inventory_container  .inventory_item")
+    .all();
+  const randomindex = Math.floor(Math.random() * items.length);
+  const index = items[randomindex];
+  const expectDecription = await index
+    .locator(".inventory_item_desc")
+    .innerText();
+  const expectName = await index.locator(".inventory_item_name").innerText();
+  const expectPrice = await index.locator(".inventory_item_price").innerText();
+  const expectPriceC = expectPrice.replace("$", "");
+  await index.getByRole("button", { name: "ADD TO CART" }).click();
+
+  await page.locator("a.shopping_cart_link").click();
+  const button_checkout = await page.getByRole("link", { name: "CHECKOUT" });
+  expect(button_checkout).toBeVisible();
+
+  const actualname = await page.locator(".inventory_item_name").innerText();
+  const description = await page.locator(".inventory_item_desc").innerText();
+  const price = await page.locator(".inventory_item_price").innerText();
+
+  expect(actualname).toEqual(expectName);
+  expect(description).toEqual(expectDecription);
+  expect(price).toEqual(expectPriceC);
+
+  button_checkout.click();
+
+  await page.getByRole("textbox", { name: "First Name" }).fill('Jhoan Sebastian');
+
+  await page.getByRole("textbox", { name: "Last Name" }).fill('Duque Zapata');
+
+  await page.getByRole("textbox", { name: "Zip/Postal Code" }).fill('12345');
+
+  await page.getByRole("button", { name: "CONTINUE" }).click();
+
+  await page.getByRole("link", {name: "FINISH"}).click();
+
+  const header = page.locator('.complete-header');
+
+  await expect(header).toBeVisible();
+
+
+  const textToCompare = await header.innerText();
+  await expect(textToCompare).toEqual('THANK YOU FOR YOUR ORDER');
+});
